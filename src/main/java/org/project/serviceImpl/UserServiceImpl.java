35e136de937b76b2fbed4c85f3fbee1e88e45325
RavimauryaWissen <ravi.maurya@wissen.com>
2025-11-23 23:36:31 +0530

    messages.properties file created

--- src/main/java/org/project/serviceImpl/UserServiceImpl.java
+++ src/main/java/org/project/serviceImpl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package org.project.serviceImpl;
 
+
 import org.project.model.User;
 import org.project.repository.UserRepository;
 import org.project.request.RegisterRequest;
 import org.project.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -11,9 +14,13 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@PropertySource("classpath:messages.properties")
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    @Value("${USERNAME.PASSWORD.REQUIRED}")
+    private String userRequiredMessage;
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -23,7 +30,7 @@ public class UserServiceImpl implements UserService {
     public User register(RegisterRequest req) {
         if (req == null || req.getUsername() == null || req.getUsername().isBlank()
                 || req.getPassword() == null || req.getPassword().isBlank()) {
-            throw new IllegalArgumentException("username and password are required");
+            throw new IllegalArgumentException(userRequiredMessage);
         }
 
         String username = req.getUsername().trim();
